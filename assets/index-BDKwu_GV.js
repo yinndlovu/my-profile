d1fc87246cdae2fe62563892d584f286d63e533e
Yin <yinnndlovu@gmail.com>
2025-08-28 09:33:35 +0200

    Updates

--- assets/index-BDKwu_GV.js
+++ assets/index-BDKwu_GV.js
@@ -42,7 +42,10 @@ graduating though, my school just needs me to complete an internship that runs
 for at least six month in order for me to be able to fulfill my graduation
 requirements.
 <br></br>
-I am highly experienced in back-end development, but I also do front-end development just as well.`,techStackExplanation:"I have worked with all these technologies",projectTwoDescription:`A mobile application built using React Native and Expo.
+I am highly experienced in back-end development, super comfortable with front-end development, 
+and also very familiar with DevOps practices. This combination allows me to not only build and 
+design applications but also understand deployment pipelines, CI/CD, and overall 
+application lifecycle management.`,techStackExplanation:"I have worked with all these technologies",projectTwoDescription:`A mobile application built using React Native and Expo.
 
 I have learned a lot from this project, it is by far my favorite project I have worked on so far.`,projectOneDescription:`This is a very cool project I recently worked on with a team from my workplace.
 We created a Lecturers' Report System for my school, which is going to be used by lecturers to submit their reports
